404e5c7c793384d6a9a825beef71663c0e76e0bd
Kate <kate.dubitski@gmail.com>
2018-01-23 22:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   var placeInLine = (katzDeliLine[name]+1);
-  return `Welcome, $(name). You are number $(placeInLine) in line.`;
+  return "Welcome, " + name +". You are number " + placeInLine + " in line.";
 }
