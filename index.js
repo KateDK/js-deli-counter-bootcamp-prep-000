17c7e2c6bce51f4c996a3c1577cd6f972b55b05a
Kate <kate.dubitski@gmail.com>
2018-01-23 22:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, " + name +". You are number " + (placeInLine+1) + " in line.";
 }
 
-function nowServing(){
+function nowServing(katzDeli){
   if(katzDeli.length === 0){
     return "There is nobody waiting to be served!";
   }
